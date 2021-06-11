e08155fc4b7e4e302f2e1d67b2833182ce152437
= <=>
2021-06-11 02:20:03 -0400

    revert

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,6 +1,6 @@
-import { Stack, StackProps, Construct } from '@aws-cdk/core';
+import { Stack, StackProps, Construct, SecretValue } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
-import * as secretsManager from '@aws-cdk/aws-secretsmanager'
+
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 
@@ -18,12 +18,9 @@ export class CdkPipelineStack extends Stack {
       sourceAction: new codepipeline_actions.GitHubSourceAction({
         actionName: 'GitHub',
         output: sourceArtifact,
-        oauthToken: secretsManager.Secret.fromSecretNameV2(
-          this,
-          'Secret From Name',
-          'github/pat'
-        ),
+        oauthToken: SecretValue.secretsManager('github/pat'),
         trigger: codepipeline_actions.GitHubTrigger.POLL,
+        // Replace these with your actual GitHub project info
         owner: 'jp4g',
         repo: 'cdk-pipeline',
       }),
@@ -31,6 +28,9 @@ export class CdkPipelineStack extends Stack {
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact,
         cloudAssemblyArtifact,
+
+        // Use this if you need a build step (if you're not using ts-node
+        // or if you have TypeScript Lambdas that need to be compiled).
         buildCommand: 'npm run build',
       }),
     });
